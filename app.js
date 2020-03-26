6c12a4ad9745d6e95c35df75b6a36a5d9198fd3e
garret.valentino@gmail.com <garret.valentino@gmail.com>
2020-03-26 12:30:39 -0500

    this the the first commit of the new project it has mostly the starter code but i added to the person info fuction

--- app.js
+++ app.js
@@ -12,21 +12,9 @@ function app(people){
       searchResults = searchByName(people);
       break;
     case 'no':
-      promptFor("Would you like to search for one or multiple traits up to 5?");
-      switch(searchType){
-      case 'one':
-       searchResults = searchByTrait(people);
-       break;
-       case 'multiple':
-         searchResults = searchByTraits(people);
-         break;
-      }
-    
       // TODO: search by traits
-    
       break;
       default:
-    
     app(people); // restart app
       break;
   }
@@ -50,6 +38,7 @@ function mainMenu(person, people){
   switch(displayOption){
     case "info":
     // TODO: get person's info
+    consol.log(displayPerson(person));
     break;
     case "family":
     // TODO: get person's family
@@ -82,38 +71,6 @@ function searchByName(people){
   // TODO: find the person using the name they entered
   return foundPerson;
 }
-function searchByMultipleTraits(people){
-
-
-}
-
-function searchByTrait(people){
-  let gender = promptFor("Please enter a trait of gender, dob, height, weight, or eye color", chars);
-
-  let foundPerson = people.filter(function(person){
-    if(person.gender === gender){
-      return true;
-    }
-    else if (person.dob === dob){
-      return true;
-    }
-    else if (person.height === height){
-      return true;
-    }
-    else if(person.weight === weight){
-      return true;
-    }
-    else if(person.eyeColor === eyeColor){
-      return true;
-    }
-    else{
-      return false;
-    }
-    
-  })
-  return foundPerson;
-
-}
 
 // alerts a list of people
 function displayPeople(people){
@@ -125,8 +82,16 @@ function displayPeople(people){
 function displayPerson(person){
   // print all of the information about a person:
   // height, weight, age, name, occupation, eye color.
-  let personInfo = "First Name: " + person.firstName + "\n";
-  personInfo += "Last Name: " + person.lastName + "\n";
+  let personInfo = "Name: " + person.firstName + personInfo + "Last Name: " + person.lastName + "\n";
+  personInfo += "Height: " + person.height + "\n";
+  personInfo += "Weight: " + person.weight + "\n";
+  personInfo += "Age: " + person.age + "\n";
+  personInfo += "Gender: " + person.gender + "\n";
+  personInfo += "DOB: " + person.dob + "\n";
+  personInfo += "Eye Color: " + person.eyeColor + "\n";
+  personInfo += "Occupation: " + person.occupation + "\n";
+  personInfo += "Parents: " + person.parents + "\n";
+  personInfo += "Current Spouse: " + person.currentSpouse + "\n"
   // TODO: finish getting the rest of the information to display
   alert(personInfo);
 }
